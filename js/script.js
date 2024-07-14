9f0d97344b71ada6ff75237bc5bd9a8c27e783f1
Hanazraismi <hanazrarare29@gmail.com>
2024-07-14 17:31:38 +0700

    run

--- js/script.js
+++ js/script.js
@@ -11,21 +11,3 @@ document.addEventListener("click", function (e) {
     navbarNav.classList.remove("active");
   }
 });
-
-const wrapper = document.querySelector(".wrapper"),
-musicImg = wrapper.querySelector(".img img"),
-musicName = wrapper.querySelector(".song .name"),
-musicArtist = wrapper.querySelector(".song .artist"),
-
-let musicIndex = 2
-
-window.addEventListener("load", ()=>{
-    loadMusic(musicIndex);
-})
-
-// load music
-
-function loadMusic(indexNumb){
-    musicName.innerText = allMusic[indexNumb - 1].name;
-    musicArtist.innerText = allMusic[indexNumb - 1].artist;
-}
